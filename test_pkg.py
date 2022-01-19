75f446f58557ed7338e0d0465c7a6aa829292834
Artem IG <ortemeo@gmail.com>
2022-01-19 22:21:36 +0300

    upd

--- test_pkg.py
+++ test_pkg.py
@@ -2,9 +2,7 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('from spacy_installer import load_model,'
-                            ' install_model, uninstall_model,'
-                            ' uninstall_all_models, can_load')
+        pkg.run_python_code('from linecompress import LinesFile')
 
     print("\nPackage is OK!")
 
