0082d6025b3745d2ef4ff4d422fd6c828f56baed
Artem IG <ortemeo@gmail.com>
2022-02-05 20:51:18 +0300

    upd

--- test_pkg.py
+++ test_pkg.py
@@ -2,7 +2,7 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('from linecompress import LinesFile')
+        pkg.run_python_code('from linecompress import LinesDir')
 
     print("\nPackage is OK!")
 
